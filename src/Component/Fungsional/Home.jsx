0179d781ee9c8b5d640c4e4038ac43030a6858d0
Michael Oki <oki.mikael@outlook.com>
2020-06-16 21:08:37 +0700

    Membuat state

--- src/Component/Fungsional/Home.jsx
+++ src/Component/Fungsional/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-import Blog from './Blog'
+//import Blog from './Blog'
+import Produk from '../Class/Produk';
 
 const Home = () => {
     return <div>
-        <Blog
+        {
+            <div>
+            <Produk nama="Macbook Pro 2010" harga="33000000" stock="10"/>
+            <Produk nama="Macbook Pro 2011" harga="34000000" stock="15"/>
+            <Produk nama="Macbook Pro 2012" harga="40000000" stock="24"/>
+            <Produk nama="Macbook Pro 2013" harga="45000000" stock="4"/>
+            <Produk nama="Macbook Pro 2014" harga="55000000" stock="123"/>
+            </div>
+        /*<Blog
             tanggal="16 Juni 2020"
             judul="Teknologi Blockchain"
             summary="Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator." />
@@ -17,7 +26,7 @@ const Home = () => {
             tanggal="18 Juni 2020"
             judul="Teknologi Laravel"
             summary="Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator." />
-
+        */}
     </div>
 }
 
