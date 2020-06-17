ec39953c4033b2c56a390ccacb0dbda7a72d7e08
Michael Oki <oki.mikael@outlook.com>
2020-06-17 21:41:27 +0700

    Membuat Parent, Child and GrandChild

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css';
-import Home from './Component/Fungsional/Home';
+//import './App.css';
+//import Home from './Component/Fungsional/Home';
+import Parent from './Component/Class/Parent';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Parent/>
     </div>
   );
 }
