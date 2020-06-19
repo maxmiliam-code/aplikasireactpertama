18bca54a67bd78267060f874f975d3a983930298
Michael Oki <oki.mikael@outlook.com>
2020-06-19 13:17:14 +0700

    menginstall axios , querystring dan mengambil (GET) data REST API

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import NavbarComp from './Component/Fungsional/NavbarComp';
 //import BootstrapComp from './Component/Class/BootstrapComp';
 import HomePage from './Component/Fungsional/HomePage';
 import About from './Component/Fungsional/AboutComp';
-import DetailComp from './Component/Fungsional/DetailComp';
+//import DetailComp from './Component/Fungsional/DetailComp';
+import ListComp from './Component/Class/ListComp';
 //import './App.css';
 //import Home from './Component/Fungsional/Home';
 //import Parent from './Component/Class/Parent';
@@ -16,7 +17,8 @@ const App = () => {
     <Switch>
       <Route exact path="/" component={HomePage}/>
       <Route exact path="/about" component={About}/>
-      <Route exact path="/detail/:id" component={DetailComp}/>
+      <Route exact path="/mahasiswa" component={ListComp}/>
+      {/*<Route exact path="/detail/:id" component={DetailComp}/>*/}
     </Switch>
     </BrowserRouter>
       
--- src/Component/Fungsional/NavbarComp.jsx
+++ src/Component/Fungsional/NavbarComp.jsx
@@ -27,6 +27,9 @@ const NavbarComp = (props) => {
                         <NavItem>
                             <NavLink href="/about">About</NavLink>
                         </NavItem>
+                        <NavItem>
+                            <NavLink href="/mahasiswa">Mahasiswa</NavLink>
+                        </NavItem>
                         
                     </Nav>
                     <NavbarText>Simple Text</NavbarText>
