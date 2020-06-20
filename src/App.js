dcfc9e432b3eaf38c91b109aa25f6f4e1d47d7f3
Michael Oki <oki.mikael@outlook.com>
2020-06-20 12:39:41 +0700

    Post Data Rest API

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HomePage from './Component/Fungsional/HomePage';
 import About from './Component/Fungsional/AboutComp';
 //import DetailComp from './Component/Fungsional/DetailComp';
 import ListComp from './Component/Class/ListComp';
+import TambahComp from './Component/Class/TambahComp';
 //import './App.css';
 //import Home from './Component/Fungsional/Home';
 //import Parent from './Component/Class/Parent';
@@ -18,6 +19,7 @@ const App = () => {
       <Route exact path="/" component={HomePage}/>
       <Route exact path="/about" component={About}/>
       <Route exact path="/mahasiswa" component={ListComp}/>
+      <Route exact path="/mahasiswa/tambah" component={TambahComp}/>
       {/*<Route exact path="/detail/:id" component={DetailComp}/>*/}
     </Switch>
     </BrowserRouter>
