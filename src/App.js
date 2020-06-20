1db379db37c7a5501cd86e762fa8d42664f22808
Michael Oki <oki.mikael@outlook.com>
2020-06-20 13:08:59 +0700

    Put Data REST API

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from './Component/Fungsional/AboutComp';
 //import DetailComp from './Component/Fungsional/DetailComp';
 import ListComp from './Component/Class/ListComp';
 import TambahComp from './Component/Class/TambahComp';
+import EditComp from './Component/Class/EditComp';
 //import './App.css';
 //import Home from './Component/Fungsional/Home';
 //import Parent from './Component/Class/Parent';
@@ -20,6 +21,7 @@ const App = () => {
       <Route exact path="/about" component={About}/>
       <Route exact path="/mahasiswa" component={ListComp}/>
       <Route exact path="/mahasiswa/tambah" component={TambahComp}/>
+      <Route exact path="/mahasiswa/edit" component={EditComp}/>
       {/*<Route exact path="/detail/:id" component={DetailComp}/>*/}
     </Switch>
     </BrowserRouter>
